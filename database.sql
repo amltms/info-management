8cac8626be893a9d4b9b0f231f7bf9b6bcfe5284
amltms <amalthomas587@gmail.com>
2019-03-15 11:02:48 +0000

    sql update

--- database.sql
+++ database.sql
@@ -1,9 +1,16 @@
-CREATE TABLE User (
-    StudentID int NOT NULL PRIMARY KEY,
-    Password varchar(255) NOT NULL,
+CREATE TABLE Lecturer (
+    LecturerID int NOT NULL PRIMARY KEY,
     FirstName varchar(255) NOT NULL,
     LastName varchar(255) NOT NULL,
+    Password varchar(255) NOT NULL,
+    Email varchar(255) NOT NULL
+);
+
+CREATE TABLE Student (
+    StudentID int NOT NULL PRIMARY KEY,
+    FirstName varchar(255) NOT NULL,
+    LastName varchar(255) NOT NULL,
+    Password varchar(255) NOT NULL,
     Email varchar(255) NOT NULL,
-    Role varchar(255) NOT NULL,
     Lecturer varchar(255)
 );
