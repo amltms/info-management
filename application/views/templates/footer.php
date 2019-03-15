28eb94ef265122ec666265f4251717d5b99aca38
josheey3 <joshburtsal@gmail.com>
2019-03-15 14:31:36 +0000

    Uploaded MVC
    + Added MVC

---
+++ application/views/templates/footer.php
@@ -0,0 +1,4 @@
+		<hr>
+		<em>&copy; 2019</em>
+	</body>
+</html>
---
+++ application/views/templates/header.php
@@ -0,0 +1,8 @@
+<html>
+	<head>
+		<title>CodeIgniter Lab wk32 tutorial</title>
+	</head>
+	
+	<body>
+		<h1><?php echo $title; ?></h1>
+		<hr>
