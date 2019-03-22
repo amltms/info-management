7a72bfe68a27e7008038986156fb089645e93073
amltms <amalthomas587>
2019-03-22 16:00:58 +0000

    +started insert script ~created the table

---
+++ databases/insert.sql
@@ -0,0 +1,7 @@
+/*Insert Roles*/
+INSERT INTO Roles (RoleName) VALUES ("Student");
+INSERT INTO Roles (RoleName) VALUES ("Lecturer");
+INSERT INTO Roles (RoleName) VALUES ("Admin");
+
+
+INSERT INTO Users (FirstName,LastName,Password,PhoneNumber,Email,Bio,RoleID,Privacy) VALUES ("Admin");
