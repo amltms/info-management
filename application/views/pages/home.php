7a7107b10423e7b5ddffb80c004131778dd497bb
josheey3 <joshburtsal@gmail.com>
2019-03-22 16:08:34 +0000

    Added login helper

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -2,5 +2,7 @@
 	Logged in as <?php echo $_SESSION['username']; ?>, this is the home page.
 <?php } else { ?>
 	Welcome guest, this is the home page
-	
-<?php } ?>
+<?php date_default_timezone_set('Europe/London');
+	$dateTime = date('Y-m-d H:i:s', time());
+	echo $dateTime;
+} ?>
