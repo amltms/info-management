7c3f487088d4835876013705a15b2a4c14c9faf5
josheey3 <joshburtsal@gmail.com>
2019-03-28 18:16:04 +0000

    Updated login and regisrer

--- application/views/pages/login.php
+++ application/views/pages/login.php
@@ -1,7 +1,15 @@
 <?php // Add custom login CSS ?>
 <link rel="stylesheet" type="text/css" href="<?php echo css_url(); ?>login.css">
-<?php echo form_open("login/login"); ?>
-	<input type="text" name="usernameInput" placeholder="Username or email address"> <br>
-	<input type="password" name="passwordInput" placeholder="Password"> <br>
-	<input type="submit" name="loginButton" value="Login">
-</form>
+<div class="center">
+	<h3>Login to UniChat</h3>
+	<?php echo form_open("login/login"); ?>
+		<input type="text" name="usernameInput" placeholder="Username or email address"> <br>
+		<input type="password" name="passwordInput" placeholder="Password"> <br>
+		<input type="submit" name="loginButton" value="Login"> <br>
+	</form>
+	<hr>
+	or
+	<form method="get" action="<?php echo page_url(); ?>register">
+		<button type="submit">Click to create an account</button>
+	</form>
+</div>
--- application/views/pages/register.php
+++ application/views/pages/register.php
@@ -1,17 +1,13 @@
-		<?php // Add custom login/register CSS ?>
-		<link rel="stylesheet" type="text/css" href="<?php echo css_url(); ?>login.css">
-		<div class="login">
-			<h3>Create an account</h3>
-			<form method="post">
-				<input type="text" name="nameInput" placeholder="Name"> <br>
-				<input type="text" name="usernameInput" placeholder="Username"> <br>
-				<input type="text" name="emailInput" placeholder="Email address"> <br>
-				<input type="password" name="passwordInput" placeholder="Password"> <br>
-				<input type="password" name="confirmPasswordInput" placeholder="Confirm Password"> <br>
-				<input type="submit" name="registerButton" value="Create account"> <br>
-			</form>
-			<?php if (isset($_POST["usernameInput"]) && isset($_POST["passwordInput"])) { // If username and password textbox have values 
-					$algorithm = "sha256"; // Which hashing algorithm should be used
-					$pwrdHash = hash($algorithm, $_POST["passwordInput"]); // Hash passwords so they are not sent as plain text
-				} ?>
-		</div>
+<?php // Add custom register/login CSS ?>
+<link rel="stylesheet" type="text/css" href="<?php echo css_url(); ?>login.css">
+<div class="center">
+	<h3>UniChat create account</h3>
+	<?php echo form_open("register/register"); ?>
+		<input type="text" name="firstNameInput" placeholder="First name"> <br>
+		<input type="text" name="secondNameInput" placeholder="Second name"> <br>
+		<input type="text" name="emailInput" placeholder="Email address"> <br>
+		<input type="password" name="passwordInput" placeholder="Password"> <br>
+		<input type="password" name="confirmPasswordInput" placeholder="Confirm password"> <br>
+		<input type="submit" name="registerButton" value="Create account"> <br>
+	</form>
+</div>
