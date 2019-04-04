7b7d66cae598e83e4a28bdc4355027ac738bd863
amltms <amalthomas587>
2019-04-04 16:27:40 +0100

    added message view

---
+++ application/views/messages/view.php
@@ -0,0 +1,5 @@
+<?php
+  echo '<h2>'.$message['MessageTitle'].' - '.$message['message'].'</h2>';
+  echo 'From: '.$sender['FirstName'].' '.$sender['LastName'].'<br>';
+  echo "<button>Reply</button>";
+?>
